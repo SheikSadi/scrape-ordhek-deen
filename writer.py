5631a0dc518ce53a1313711704c944894c9090d7
Sheik Sadi Rohmotullah <34588815+SheikSadi@users.noreply.github.com>
2022-06-18 02:11:43 +0600

    Update writer.py

--- writer.py
+++ writer.py
@@ -26,7 +26,7 @@ def write():
                 fp.write(
                     str(biodata_number)
                 )
-
+        break
 
 if __name__ == "__main__":
     if not os.path.isdir("data"):
